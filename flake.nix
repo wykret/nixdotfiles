d28e3d9cd82edb45a598ef42d47ae7f8cef92472
wykret <wykret@github.com>
2026-03-02 03:07:49 -0300

    .

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Lucas NixOS config";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-25.11";
 
     home-manager = {
       url = "github:nix-community/home-manager";
